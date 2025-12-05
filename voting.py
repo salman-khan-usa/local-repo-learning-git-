a43101fde98e7b713cc3582aea81c6c1ed8e6580
salman-khan-usa <salmankhanniazi706@gmail.com>
2025-12-05 10:48:33 +0500

    voting.py

---
+++ voting.py
@@ -0,0 +1,11 @@
+age = int('Enter your age: ')
+
+if age >= 18:
+    print('Your Allowed to vote bro...')
+
+elif age == 17:
+    print('You can vote from next year')
+
+else:
+    print('You cant vote')    
+    
